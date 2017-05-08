38b587bb0db7a4f661551b2dbe9b17e34ced0184
ttosi <ttosi519@gmail.com>
2017-05-07 17:55:38 -0700

    vuejs exercise4

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 new Vue({
-    el: '#app',
+    el: '#exercise',
     data: {
-        title: 'Hello, world!'
+        hasEffect: false,
+        userClass: '',
+        useUserClass: false,
+        userStyle: 'red',
+        iveClass: 'ive',
+        gotClass: 'got',
+        classClass: 'class',
+        progressWidth: 0
     },
     methods: {
-        changeTitle: function(e) {
-            this.title = e.target.value
-        }
-    } 
+        startEffect() {
+            this.hasEffect = true; // start the effect change immediately
+            setInterval(() => {
+                this.hasEffect = !this.hasEffect
+            }, 750);
+        },
+        startProgress: function () {
+            this.progressWidth = 0;
+            let progressInterval = setInterval(()=> {
+                this.progressWidth += .25;
+                if(this.progressWidth === 100) {
+                    clearInterval(progressInterval);
+                }
+            }, 8);
+        },
+    }
 });
